e112cf532c4acd9bccc76787e0b275399b0e2ea1
TechWiz-3 <programmingplus1@gmail.com>
2022-09-21 13:06:04 +1000

    📦 NEW: main file

--- test.py
+++ test.py
@@ -5,20 +5,32 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.rule import Rule
 from rich.status import Status
+import sys
 
 # create a super pretty one with a --nerd-fonts option
 
-panels = True
+panels = False
 bubbles = False
+simple = False  # no colour
+if sys.argv[1] == "panels":
+    panels = True
+if sys.argv[1] == "bubbles":
+    bubbles = True
 simple = False
 
 info = [("rafa", "https://github.com/rafa"), ("cortex", "https://github.com/cortex")]
 info = []
 console = Console()
 
+def get_inverse(bg_col, txt):
+    circle_style=f"[{bg_col}]"
+    circle_close = circle_style[:1] + "/" + circle_style[1:]
+    bubble = f"{circle_style}{circle_close}{txt}{circle_style}{circle_close}"
+    return bubble
+
 def get_links():
     # pretend get links
-    time.sleep(3)
+    time.sleep(1)
     global stop_threads
     stop_threads = True
     tasks = range(10)
@@ -37,6 +49,9 @@ def print_get():
             spinner.stop()
             if panels:
                 txt = Panel.fit("[green]✔ [underline]Fetched github followers")
+            elif bubbles:
+                txt = get_inverse("cyan", "[white on cyan]Fetched github\
+ followers[/white on cyan]")
             elif not panels:
                 print("")
                 txt = "[green]✔ [underline]Fetched github followers"
@@ -58,9 +73,24 @@ def no_unfollows():
 \n", subtitle=":pray: Thanks for using this project", subtitle_align="left")
         console.print(txt_b)
         print("\n")
+    elif bubbles:
+        print("")
+        txt_a = get_inverse("green4", "[white on green4]No \
+unfollows![/white on green4]"
+               )
+        console.print(":thumbs_up:", txt_a)
+        print("")
+        txt_b = get_inverse("purple", "[white on purple]You have 80 followers.[/white on purple]")
+        text_c = get_inverse("magenta", "[white on magenta]Keep up the good\
+ work![/white on magenta]")
+        console.print(":fire:", txt_b, text_c)
+        print("")
+        txt = get_inverse("blue", "[white on blue]Thanks for using this project[/white on blue]")
+        console.print(":pray:", txt)
+        print("")
     elif not panels:
         print("")
-        txt_a = ":raised_hands: [white on #308012]No unfollows![/white on #308012]                                "
+        txt_a = "[green]:raised_hands: [underline]No unfollows!"
         console.print(txt_a)
         print("")
         txt_b = Panel.fit(":fire: You have 80 followers. Keep up the good work\
@@ -73,11 +103,21 @@ def no_unfollows():
 def start():
     if panels:
         txt = Panel.fit(
-            ":dancer: [purple]Welcome to [/purple] [red]who-unfollowed-me[/red][blue] python implementation[/blue] by [#FFD700]Zac the Wise[#FFD700]"
+            ":dancer: [purple]Welcome to[/purple] [red]who-unfollowed-me[/red][blue] python implementation[/blue] by [#FFD700]Zac the Wise[#FFD700]"
         )
+    elif bubbles:
+        print("")
+        part_a = get_inverse("purple", "[white on purple]Welcome to[/white on purple]")
+        part_b = get_inverse("red", "[white on red]who-unfollowed-me[/white on red]")
+        part_c = get_inverse("blue", "[white on blue]the Python implementation[/white on blue]")
+        part_d = get_inverse("dark_goldenrod", "[white on dark_goldenrod]by Zac\
+ the Wise[/white on dark_goldenrod]")
+        txt = f":dancer: {part_a} {part_b} {part_c} {part_d}"
+        #txt = get_inverse("purple", "[on purple]:dancer: [purple]Welcome to[/purple] [red]who-unfollowed-me[/red][blue] python implementation[/blue] by [#FFD700]Zac the Wise[#FFD700]"
+#)
     elif not panels:
         print("")
-        txt = ":dancer: [purple]Welcome to [/purple] [red]who-unfollowed-me[/red][blue] python implementation[/blue] by [#FFD700]Zac the Wise[#FFD700]"
+        txt = ":dancer: [purple]Welcome to[/purple] [red]who-unfollowed-me[/red][blue] python implementation[/blue] by [#FFD700]Zac the Wise[#FFD700]"
     console.print(txt)
 
 
