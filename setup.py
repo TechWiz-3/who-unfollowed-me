2236fcb1b2f7619674d184e30b555d1c30a7359c
TechWiz-3 <programmingplus1@gmail.com>
2022-09-25 00:43:36 +1000

    📦 NEW: setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from pathlib import Path
+
+#project_dir = Path(__file__).parent
+#long_description = (project_dir / "README.md").read_text()
+
+setup(
+    name="unfollow",
+    url="https://github.com/TechWiz-3/who-unfollowed-me",
+    author="Zac the Wise aka TechWiz-3",
+    version='1.0.0',
+    description="😡 Python CLI tool that shows you who has unfollowed you on GitHub",
+#    long_description_content_type='text/markdown',
+#    long_description=long_description,
+    scripts=["unfollow"]
+)
