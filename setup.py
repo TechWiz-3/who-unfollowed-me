e65ef585e674d32766cd517c68fa165dc358a3a3
TechWiz-3 <programmingplus1@gmail.com>
2022-09-25 03:32:18 +1000

    🔖 v1.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="unfollow",
     url="https://github.com/TechWiz-3/who-unfollowed-me",
     author="Zac the Wise aka TechWiz-3",
-    version='1.0.3',
+    version='1.0.4',
     description="😡 Python CLI tool that shows you who has unfollowed you on GitHub",
     packages=find_packages(
         where='src',
