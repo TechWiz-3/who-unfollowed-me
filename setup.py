6781f5d3f8dd74b04206385579d5cc94497a2455
TechWiz-3 <programmingplus1@gmail.com>
2022-10-08 23:19:55 +1100

    👌 IMPROVE: setup

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     name="unfollow",
     url="https://github.com/TechWiz-3/who-unfollowed-me",
     author="Zac the Wise aka TechWiz-3",
-    version='1.0.4',
+    version='2.0.0',
     description="😡 Python CLI tool that shows you who has unfollowed you on GitHub",
-    packages=find_packages(
-        where='src',
-    ),
-    package_dir={"":"src"},
+    packages=find_packages(),
 #    long_description_content_type='text/markdown',
 #    long_description=long_description,
-    scripts=["unfollow"],
+    entry_points='''
+    [console_scripts]
+    unfollow=unfollow.cli:main
+    ''',
     install_requires=['rich'],
 )
