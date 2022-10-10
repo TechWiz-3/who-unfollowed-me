f56cee4cc7bfa05e155785a83f70d024dbcd7f1a
tintinthong <justinthong93@gmail.com>
2022-10-10 23:27:09 +0800

    black formatting

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(
     name="unfollow",
     url="https://github.com/TechWiz-3/who-unfollowed-me",
     author="Zac the Wise aka TechWiz-3",
-    version='2.0.0',
+    version="2.0.0",
     description="😡 Python CLI tool that shows you who has unfollowed you on GitHub",
     packages=find_packages(),
-#    long_description_content_type='text/markdown',
-#    long_description=long_description,
-    entry_points='''
+    #    long_description_content_type='text/markdown',
+    #    long_description=long_description,
+    entry_points="""
     [console_scripts]
     unfollow=unfollow.cli:main
-    ''',
-    install_requires=['rich'],
+    """,
+    install_requires=["rich"],
 )
--- unfollow/cli.py
+++ unfollow/cli.py
@@ -16,10 +16,10 @@ console = Console()
 
 config = get_config()
 
-theme = config['apperance']['styling']['theme']
-emojis = config['apperance']['emojis']
+theme = config["apperance"]["styling"]["theme"]
+emojis = config["apperance"]["emojis"]
 
-locale_lang = config['locale']['locale']
+locale_lang = config["locale"]["locale"]
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 help_txt = "[optional]\n'regular':  regular theme (triggers if no other theme\
@@ -27,13 +27,18 @@ help_txt = "[optional]\n'regular':  regular theme (triggers if no other theme\
  everything in panels\n'bubbles':  displays everything in\
  bubbles (requires a nerd font to be used)\n'regular':  \
 default, some panels used\n'simple':   no color"
-parser.add_argument('style', nargs='?', help=help_txt)
+parser.add_argument("style", nargs="?", help=help_txt)
 
-parser.add_argument('--token', action="store_true",
-                    help="Uses an env variable stored as\
- UNFOLLOW_TOKEN which is a github token for requests to the API.")
-parser.add_argument('--test', action="store_true", help="for testing purposes")
-parser.add_argument('--cached', action="store_true", help="cache followers since last week")
+parser.add_argument(
+    "--token",
+    action="store_true",
+    help="Uses an env variable stored as\
+ UNFOLLOW_TOKEN which is a github token for requests to the API.",
+)
+parser.add_argument("--test", action="store_true", help="for testing purposes")
+parser.add_argument(
+    "--cached", action="store_true", help="cache followers since last week"
+)
 
 args = parser.parse_args(sys.argv[1:])
 
@@ -65,7 +70,7 @@ elif args.style == "regular":
     theme = "regular"
     regular = True
 
-locale = config['locale'][locale_lang][theme]
+locale = config["locale"][locale_lang][theme]
 
 
 def get_inverse(bg_col, txt, txt_before=None):
@@ -91,31 +96,34 @@ def print_get():
     global bubbles
     global simple
     if panels:
-        txt = Panel.fit(locale['fetched_followers_message'])
+        txt = Panel.fit(locale["fetched_followers_message"])
         sys.stdout.write("\033[F")  # lift the line up
     elif bubbles:
-        txt = get_inverse("cyan", locale['fetched_followers_message'], txt_before=":mag:")
+        txt = get_inverse(
+            "cyan", locale["fetched_followers_message"], txt_before=":mag:"
+        )
         # end = "\r\n"
     elif regular:
-        txt = locale['fetched_followers_message']
+        txt = locale["fetched_followers_message"]
     console.print(txt)
     return
 
 
 def no_unfollows():
     from rich.style import Style
+
     a = Style(color="green")
     if panels:
-        txt = Panel.fit(locale['no_unfollows_message'], border_style=a)
+        txt = Panel.fit(locale["no_unfollows_message"], border_style=a)
         console.print(txt)
     elif bubbles:
         print("")
-        txt = get_inverse("green4", locale['no_unfollows_message'])
-        console.print(emojis['no_unfollows_emoji'], txt)
+        txt = get_inverse("green4", locale["no_unfollows_message"])
+        console.print(emojis["no_unfollows_emoji"], txt)
         print("")
     elif regular:
         print("")
-        txt_a = locale['no_unfollows_message']
+        txt_a = locale["no_unfollows_message"]
         console.print(txt_a)
 
 
@@ -124,26 +132,28 @@ def end(follower_num=0):  # remember TO CHANge THIS
     # console.print("[#026440 on black][/#026440 on black]Yay we all g bruh[#026440 on black]", style="white on #026440")
     if panels:
         txt_b = Panel.fit(
-            locale['end_message'].format(follower_num=follower_num),
-            subtitle=locale['thankyou_message'],
-            subtitle_align="left"
+            locale["end_message"].format(follower_num=follower_num),
+            subtitle=locale["thankyou_message"],
+            subtitle_align="left",
         )
         console.print(txt_b)
         print("\n")
     elif bubbles:
-        txt_b = get_inverse("purple", locale['end_message_a'].format(follower_num=follower_num))
-        text_c = get_inverse("magenta", locale['end_message_b'])
-        console.print(emojis['follow_count_emoji'], txt_b, text_c)
+        txt_b = get_inverse(
+            "purple", locale["end_message_a"].format(follower_num=follower_num)
+        )
+        text_c = get_inverse("magenta", locale["end_message_b"])
+        console.print(emojis["follow_count_emoji"], txt_b, text_c)
         print("")
-        txt = get_inverse("blue", locale['thankyou_message'])
-        console.print(emojis['thankyou_emoji'], txt)
+        txt = get_inverse("blue", locale["thankyou_message"])
+        console.print(emojis["thankyou_emoji"], txt)
         print("")
     elif regular:
         print("")
         txt_b = Panel.fit(
-            locale['end_message'].format(follower_num=follower_num),
+            locale["end_message"].format(follower_num=follower_num),
             subtitle=":pray: Thanks for using this project",
-            subtitle_align="left"
+            subtitle_align="left",
         )
         console.print(txt_b)
         print("\n")
@@ -151,17 +161,17 @@ def end(follower_num=0):  # remember TO CHANge THIS
 
 def start():
     if panels:
-        txt = Panel.fit(locale['welcome_message'])
+        txt = Panel.fit(locale["welcome_message"])
     elif bubbles:
         print("")
-        part_a = get_inverse("purple", str(locale['welcome_message_a']))
-        part_b = get_inverse("red", locale['welcome_message_b'])
-        part_c = get_inverse("blue", locale['welcome_message_c'])
-        part_d = get_inverse("dark_goldenrod", locale['welcome_message_d'])
+        part_a = get_inverse("purple", str(locale["welcome_message_a"]))
+        part_b = get_inverse("red", locale["welcome_message_b"])
+        part_c = get_inverse("blue", locale["welcome_message_c"])
+        part_d = get_inverse("dark_goldenrod", locale["welcome_message_d"])
         txt = f"{emojis['init_emoji']} {part_a} {part_b} {part_c} {part_d}"
     elif regular:
         print("")
-        txt = locale['welcome_message']
+        txt = locale["welcome_message"]
     console.print(txt)
 
 
@@ -172,7 +182,9 @@ def check_connectivity():
         requests.get(url, timeout=timeout)
     except (requests.ConnectionError, requests.Timeout):
         print("Internet connection invalid, please try again later!")
-        Console().print("If you think this is a bug, please make an issue.", style="dim")
+        Console().print(
+            "If you think this is a bug, please make an issue.", style="dim"
+        )
         exit(1222)  # microsoft ERROR_NO_NETWORK code coz why not
 
 
@@ -185,20 +197,20 @@ def main():
         # follows saved for later
         end(follower_num=info[1])
     elif info[0] == "regular":
-        if len(info[2])> 0:# unfollowers have been detected
+        if len(info[2]) > 0:  # unfollowers have been detected
             if bubbles:
-                beautify_unfollows(info[2],special="bubbles")
+                beautify_unfollows(info[2], special="bubbles")
             elif panels:
                 beautify_unfollows(info[2], special="panels")
             else:
                 beautify_unfollows(info[2])
         else:
             no_unfollows()
-        txt = Panel.fit(locale['last_week_unfollowers'])
+        txt = Panel.fit(locale["last_week_unfollowers"])
         console.print(txt)
-        if len(info[3])> 0:# unfollowers have been detected
+        if len(info[3]) > 0:  # unfollowers have been detected
             if bubbles:
-                beautify_unfollows(info[3],special="bubbles")
+                beautify_unfollows(info[3], special="bubbles")
             elif panels:
                 beautify_unfollows(info[3], special="panels")
             else:
