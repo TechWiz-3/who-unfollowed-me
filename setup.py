9575755cd4d07c0a4be1eda42902e8e589ec0e57
rexdivakar <rexdivakar@hotmail.com>
2022-10-11 08:32:45 +0530

    Make README.md compatible with Pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
+from os import path
 
-# project_dir = Path(__file__).parent
-# long_description = (project_dir / "README.md").read_text()
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+# Get the long description from the README file
+with open("README.md", encoding="utf-8") as f:
+    read_file = f.read()
+ 
 
 setup(
     name="unfollow",
@@ -9,12 +20,13 @@ setup(
     author="Zac the Wise aka TechWiz-3",
     version='2.0.0',
     description="😡 Python CLI tool that shows you who has unfollowed you on GitHub",
-    packages=find_packages(),
-#    long_description_content_type='text/markdown',
-#    long_description=long_description,
+    packages=['unfollow'],
+    long_description=read_file,
+    long_description_content_type='text/markdown',
     entry_points='''
     [console_scripts]
     unfollow=unfollow.cli:main
     ''',
+    classifiers=classifiers,
     install_requires=['rich'],
 )
