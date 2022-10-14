7140d4be736b6cafee217c9d330f332c89a32af4
Dinoosawruss <m.dormand@sky.com>
2022-10-14 11:41:51 +0200

    🐛 FIX: Ensure directory exists before creating config

--- unfollow/config.py
+++ unfollow/config.py
@@ -280,6 +280,12 @@ thankyou_message = "[white on blue]இந்தத் திட்டத்த�
 
 def get_config(overwrite=False) -> dict:
     global threads_stopped
+
+
+    # Ensure directory exists before creating file
+    if not os.path.exists(f"{UNFOLLOW_PATH}"):
+        os.mkdir(UNFOLLOW_PATH)
+
     if os.path.exists(f"{UNFOLLOW_PATH}/unfollow.toml") and not overwrite:
         config = toml.load(f"{UNFOLLOW_PATH}/unfollow.toml")
     elif overwrite:
